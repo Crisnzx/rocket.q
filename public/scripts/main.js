e8cb32e04e2b5b314ffeb32710e1409a392146f8
Crisnzx <cristovao.neto@academico.ifpb.edu.br>
2021-06-25 23:30:19 -0300

    modal javascript

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,18 +1,41 @@
 import Modal from './modal.js';
 
 const modal = Modal();
-
+// Open modal "Marcar como lida"
 // This will work in all cases
-const checkButtonsUI = document.querySelectorAll('.options a.check');
-
-checkButtonsUI.forEach((checkButton) => {
+modal.checkButtonsUI.forEach((checkButton) => {
    checkButton.addEventListener('click', (e) => {
-      modal.open();
-      console.log(e.target);
-      console.log('OI');
-      e.preventDefault();
+      handleClick(e, true);
    });
 });
+
+// Open modal "Excluir"
+modal.deleteButtonsUI.forEach((button) => {
+   button.addEventListener('click', (e) => {
+      handleClick(e, false);
+   });
+});  
+
+// Close modal
+modal.cancelButtonUI.addEventListener('click', modal.close);
+
+function handleClick(e, read) {
+   
+   if(read) {
+      modal.OKButtonUI.classList.add('blue');
+
+   } else {
+      modal.OKButtonUI.classList.remove('blue');
+      
+   }
+   const text = read ? 'Marcar como lida' : 'Excluir';
+   modal.titleUI.textContent = `${text} essa pergunta`;
+   modal.textUI.textContent = `Tem certeza que você deseja ${text.toLowerCase()} essa pergunta?`;
+   modal.OKButtonUI.value = text;
+   
+   modal.open();
+   e.preventDefault();
+}
 
 /* 
 SAME THING BUT WITH EVENT DELEGATION
--- public/scripts/modal.js
+++ public/scripts/modal.js
@@ -1,11 +1,32 @@
 // Factory
 export default function Modal() {
+   const checkButtonsUI = document.querySelectorAll('.options a.check');
+   const deleteButtonsUI = document.querySelectorAll('.options a.delete');
+
+   const modalOverlayUI = document.querySelector('.modal-overlay');
+   
+   const titleUI = document.querySelector('.modal h3');
+   const textUI = document.querySelector('.modal p');
+
+   const cancelButtonUI = document.getElementById('cancel');
+   const OKButtonUI = document.querySelector('.modal input[type="submit"]');
+
+   document.querySelector('.modal-overlay');
    function open() {
-      document.querySelector('.modal-overlay').classList.add('on');
+      modalOverlayUI.classList.add('on');
    }
    function close() {
-      document.querySelector('.modal-overlay').classList.remove('off');
+      modalOverlayUI.classList.remove('on');
    }
 
-   return { open, close }
+   return { 
+      checkButtonsUI,
+      deleteButtonsUI,
+      titleUI,
+      textUI,
+      cancelButtonUI,
+      OKButtonUI,
+      open, 
+      close 
+   }
 }
