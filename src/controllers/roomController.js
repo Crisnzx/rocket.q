7690deaa618405326fe3a02aeaae038d24ca0dba
Crisnzx <cristovao.neto@academico.ifpb.edu.br>
2021-06-26 14:58:38 -0300

    aula 04 finalizada

--- src/controllers/roomController.js
+++ src/controllers/roomController.js
@@ -1,6 +1,26 @@
+const Database = require('../db/config');
+
 module.exports = {
-   create(req, res) {
-      let roomID = 123456;
+   async create(req, res) {
+      const db = await Database();
+      const pass = req.body.password;
+      let roomID = '';
+
+      for (let i = 0; i < 6; i++) {
+         roomID += Math.floor(Math.random() * 10);
+      }
+
+      
+      await db.run(`
+         INSERT INTO rooms (
+            id,
+            pass
+         ) VALUES (
+            ${parseInt(roomID)},
+            ${pass}
+         )
+      `);
+      await db.close();
 
       res.redirect(`/room/${roomID}`);
    }
--- src/db/config.js
+++ src/db/config.js
@@ -2,8 +2,9 @@ const sqlite3 = require('sqlite3');
 const { open } = require('sqlite');
 
 module.exports = () => {
-   open({
-      filename: '/src/db/rocketq.sqlite',
+   
+   return open({
+      filename: './src/db/rocketq.sqlite',
       driver: sqlite3.Database
    });
 }
